50133002596050c488e900899e4f907c83d872aa
thilllon <thilllon970@gmail.com>
2024-07-29 15:19:10 +0900

    feat: mlops day2

---
+++ aiffel/mlops/day2/README.md
@@ -0,0 +1,23 @@
+# Database
+
+```sh
+# 도커 컴포즈를 실행하여 postgresql 을 실행한다.
+docker compose up -d
+
+
+# container shell 접속
+docker exec -it postgres1234 sh
+
+# postgresql 접속
+su - postgres
+psql -U postgres
+
+# \list
+
+```
+
+![postgres database list](image.png)
+
+```sql
+
+```
---
+++ aiffel/mlops/day2/init-postgresql.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE mydatabase;
+-- \c mydatabase;
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO
+    users (name, email)
+VALUES
+    ('Alice', 'alice@example.com'),
+    ('Bob', 'bob@example.com');
